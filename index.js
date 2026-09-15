74348a87eec88ad659de3711f565da75b20c741c
Dragana888 <dragana.andonovska28@gmail.com>
2026-09-16 00:52:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+document.cookie = "lang=mk; path=/; Secure";
+document.cookie = "delivery=none; path=/; Secure";
+document.cookie = "name=none; path=/; Secure";
+document.cookie = "email=none; path=/; Secure";
+document.cookie = "tel=none; path=/; Secure";
+
+
+function getCookie(cookieName) {
+    let name = cookieName + "=";
+    
+    let cookieArray = document.cookie.split(';');
+    
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i].trim();
+        
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+
+    return "";
+}
+
+function changeLanguage(targetLang) {
+    
+    document.cookie = `user_lang=${targetLang}; max-age=${365 * 24 * 60 * 60}; path=/; SameSite=Lax`;
+
+    let currentPath = window.location.pathname;
+  
+    let newPath;
+    if (targetLang === 'mk') {
+        newPath = currentPath.replace('/prevod/', '/mk/');
+    } else {
+        newPath = currentPath.replace('/mk/', '/prevod/');
+    }
+
+    window.location.href = newPath;
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const currentLang = window.location.pathname.includes('/mk/') ? 'mk' : 'en';
+    
+    document.cookie = `user_lang=${currentLang}; max-age=${365 * 24 * 60 * 60}; path=/; `;
+});
+
+
